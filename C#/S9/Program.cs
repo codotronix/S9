bbfbb1cd36e0399b5b3eca15dc441ef864b34680
Suman Barick <codotronix@gmail.com>
2016-03-22 11:02:41 +0530

    C# initial commit

---
+++ C#/S9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace S9_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var source =  File.ReadAllText("test_SrcCode.txt");
+
+            S9 s9Instace = S9.getS9(source);
+            s9Instace.init();
+
+            //Wait for User to see the output...
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/S9/UserVar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S9_CSharp
+{
+    class UserVar
+    {
+        public String name;             // { get; set;}
+        public String value;            // { get; set; }
+        public String level = "0";      // { get; set; }
+        public String type;             // { get; set; }
+    }
+}
